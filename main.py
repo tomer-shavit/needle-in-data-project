1d2e70cf871b6f5e21a6cb7b879894f5f84510be
tomer-shavit <tomer.shavit5@gmail.com>
2024-08-31 15:28:36 +0300

    with embeddings

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import json
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Load the CSV file
-file_path = "./Merged_DataFrame.csv"
+file_path = "./combined_cleaned_news.csv"
 df = pd.read_csv(file_path)
 
 # Assuming the CSV has a 'Title' column and an 'ID' column
 titles = df['Title'].tolist()
 ids = df['ID'].tolist()
+
 
 # Function to get vector embedding from OpenAI API
 def get_embedding(text):
@@ -20,17 +21,33 @@ def get_embedding(text):
         model="text-embedding-3-small",
         input=text
     )
+    print(f"embedded the Title '{text}'")
     return response['data'][0]['embedding']
 
-# Create a dictionary to store the ID as the key and title, vector embedding as values
-data = {}
 
+data = {}
+failed = []
 for title_id, title in zip(ids, titles):
-    embedding = get_embedding(title)
-    data[title_id] = {
-        "title": title,
-        "embedding": embedding
-    }
+    try:
+        embedding = get_embedding(title)
+        data[title_id] = {
+            "title": title,
+            "embedding": embedding
+        }
+    except:
+        print(f"failed to emmbed {title}")
+        failed.append((title_id, title))
+
+for item in failed:
+    try:
+        embedding = get_embedding(item[1])
+        data[item[0]] = {
+            "title": item[1],
+            "embedding": embedding
+        }
+    except:
+        print(f"FAILED AGAIN: '{item[1]}'")
+
 
 # Save the data to a JSON file
 output_file = "./titles_embeddings.json"
